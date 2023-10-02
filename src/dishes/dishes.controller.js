99127a83ae71f2f79330a38272eaca6fc2c47059
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-02 16:11:58 -0400

    Adding get, create, and update for dishes.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,4 +8,74 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
+function list(req, res) {
+   res.json({ data: dishes});
+  }
 
+  function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+      const { data = {} } = req.body;
+      if (data[propertyName]) {
+        return next();
+      }
+
+      
+      next({ status: 400, message: `Dish must include a ${propertyName}` });
+    };
+  }
+
+  function create(req, res){
+    const { data: { name, description, price, image_url } ={}}=req.body
+    const newDish={
+        id: {nextId},
+        name,
+        description,
+        price,
+        image_url,
+    }
+    dishes.push(newDish)
+    res.status(201).json({ data: newDish })
+
+  }
+
+  function dishExists(req, res, next){
+    const dishId =Number(req.params.dishId)
+    const foundDish=dishes.find((dish)=>dish.id===dishId)
+    if(foundDish){
+        return next()
+    }
+    next({
+        status: 404,
+        message: `Dish does not exist: ${req.params.dishId}.`,
+    })
+  }
+
+  function update(req, res){
+    const dishId =Number(req.params.dishId)
+    const foundDish=dishes.find((dish)=>dish.id===dishId)
+
+    const { data: { name, description, price, image_url } ={}}=req.body
+    foundDish.name=name
+    foundDish.description=description
+    foundDish.price=price
+    foundDish.image_url=image_url
+    res.json({ data: foundDish });
+
+  }
+
+  module.exports ={
+    list,
+    create: [  bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    create],
+    update: [bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    dishExists, 
+    update],
+
+
+  }
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,18 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+
+router
+.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed)
+
+router
+.route(":/dishId")
+.put(controller.update)
+.all(methodNotAllowed)
 
 module.exports = router;
