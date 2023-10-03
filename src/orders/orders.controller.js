f232ed7f94593713381de88578117d62d52d4f91
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-03 17:34:20 -0400

    Work on put and delete methods for order ID.

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -23,26 +23,57 @@ function bodyDataHas(propertyName) {
         }
 
 function hasQuantity(req, res, next) {
-            const { data: { quantity } = {} } = req.body;
-            if (isNaN(quantity) || price <= 0) {
-            return next({
-            status: 400,
-            message: "Dish must have a quantity that is a positive number",
-            });
-            }
-            next();
-            }
+   const { data: { dishes } = {} } = req.body
 
-function hasDishes(req, res, next){
-    const { data: { dishes } = {} } = req.body;
-    if (dishes==[] || dishes.length>0) {
+  dishes.forEach((dish) => {
+    const quantity = dish.quantity
+
+    if (!quantity || quantity <= 0 || typeof quantity !== "number") {
+      return next({
+        status: 400,
+        message: `dish ${dish.id} must have quantity property, quantity must be an integer, and it must not be equal to or less than 0`,
+      })
+    }
+  })
+  next()}
+
+
+function dishesHasValidArray(req, res, next){
+      const { data: { dishes } = {} } = req.body;
+    if (!Array.isArray(res.locals.dishes) || res.locals.dishes.length == 0) {
     return next({
     status: 400,
-    message: "dish",
+    message: "Order must include at least one dish",
     });
     }
     next();
     }
+
+
+
+function hasDishes(req, res, next){
+   const { data: { dishes } = {} } = req.body
+
+  if (dishes) {
+    res.locals.dishes = dishes
+    return next()
+  }
+  next({
+    status: 400,
+    message: `A 'dishes' property is required.`,
+  })
+}
+
+function dishesArrayIsValid(req, res, next) {
+  const { data: { dishes } = {} } = req.body
+  if (!Array.isArray(res.locals.dishes) || res.locals.dishes.length == 0) {
+    next({
+      status: 400,
+      message: `invalid dishes property: dishes property must be non-empty array`,
+    })
+  }
+  next()
+}
 
 function orderExists(req, res, next) {
                const orderId = req.params.orderId;
@@ -75,42 +106,77 @@ function read(req, res, next){
     const order = res.locals.order;
     res.json({data: order});
 }
+
+ function update(req, res, next){
+    const order = res.locals.order;
+    const {orderId} = req.params;
+     const { data: { id, deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+    if(!id || orderId === id){
+        const updatedOrder = {
+            id: orderId,
+            deliverTo,
+            mobileNumber,
+            status,
+            dishes,
+        }
+
+        res.json({data: updatedOrder});
+    }
+
+    next({
+        status: 400,
+        message: `Order id does not match route id. Dish: ${id}, Route: ${orderId}`,
+    })
+}
+
+function checkStatus(req, res, next) {
+    const { data: { status } = {} } = req.body
+  if (!status || status==="") {
+    next({
+      status: 400,
+      message: `Order must have a status of pending, preparing, out-for-delivery, delivered`,
+    })
+  }
+  else if(status==="delivered"){
+      next({
+      status: 400,
+      message: `A delivered order cannot be changed`,
+    })
+  }
+  next()
+}
+
+function destroy(req, res, next) {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === Number(orderId));
+  if (index > -1) {
+    orders.splice(index, 1);
+  }
+  res.sendStatus(204);
+}
       
      
 
-function priceExists(req, res, next){
-    const {data: {price} = {}} = req.body;
-
-    if(!price){
-        next({
-            status: 400,
-            message: "Dish must include a price",
-        })
-    }
-    else if(price <= 0){
-        next({
-            status: 400,
-            message: "Dish must have a price greater than 0.",
-        });
-    }
-    else if(typeof price != "number"){
-        next({
-            status: 400,
-            message: "The Dish price must be a number.",
-        })
-    }
-
-    return next();
-}
 
 module.exports={
         list,
         create: [bodyDataHas("deliverTo"),
         bodyDataHas("mobileNumber"),
-        bodyDataHas("status"),
         bodyDataHas("dishes"),
-        hasQuantity,
         hasDishes,
+        dishesArrayIsValid,
+        hasQuantity,
         create],
+        update: [bodyDataHas("deliverTo"),
+        bodyDataHas("mobileNumber"),
+        bodyDataHas("dishes"),
+        hasDishes,
+        dishesArrayIsValid,
+        hasQuantity,
+        orderExists,
+        checkStatus,
+        update],
         read: [orderExists, read],
+        delete: [orderExists, destroy],
     }
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -14,6 +14,8 @@ router
 router
 .route("/:orderId")
 .get(controller.read)
+.put(controller.update)
+.delete(controller.delete)
 .all(methodNotAllowed)
 
 
